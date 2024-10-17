59b6a3a61de5ebb24772497a2da0126bfd3455a0
Chaemin-L <chaem12@kookmin.ac.kr>
2024-10-17 11:09:20 +0900

    feat: post api 연동

--- src/components/RequestForm.js
+++ src/components/RequestForm.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import usePostUserApi from "../api/usePostUserApi";
+
 const RequestForm = ({ mode, setData }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [postUser] = usePostUserApi();
+
   if (mode === "get") return <></>;
   return (
     <>
@@ -12,14 +19,31 @@ const RequestForm = ({ mode, setData }) => {
 
       {mode === "post" && (
         <>
-          <input type="text" placeholder="username" />
-          <input type="number" placeholder="age" />
+          <input
+            type="text"
+            placeholder="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={() => postUser({ variables: { name, email } })}
+          >
+            전송
+          </button>
         </>
       )}
 
       {mode === "delete" && (
         <>
           <input type="number" placeholder="userId" />
+          <button type="submit">전송</button>
         </>
       )}
       {mode === "update" && (
@@ -27,10 +51,9 @@ const RequestForm = ({ mode, setData }) => {
           <input type="number" placeholder="userId" />
           <input type="text" placeholder="username" />
           <input type="number" placeholder="age" />
+          <button type="submit">전송</button>
         </>
       )}
-
-      <button type="submit">전송</button>
     </>
   );
 };
